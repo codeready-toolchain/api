e119697516d6a0681393c0e5080fab712f959157
Xavier Coulon <xcoulon@redhat.com>
2019-06-13 18:24:30 +0200

    update generated deepcopy
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/apis/toolchain/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/toolchain/v1alpha1/zz_generated.deepcopy.go
@@ -56,7 +56,7 @@ func (in *MasterUserRecord) DeepCopyObject() runtime.Object {
 func (in *MasterUserRecordList) DeepCopyInto(out *MasterUserRecordList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	out.ListMeta = in.ListMeta
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]MasterUserRecord, len(*in))
