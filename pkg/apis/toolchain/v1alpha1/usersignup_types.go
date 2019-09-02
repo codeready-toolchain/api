bc35761fdeee506d749e3850b6d3cbc40df6c43a
Shane Bryzak <sbryzak@gmail.com>
2019-09-03 07:38:26 +1000

    CRT-225 added printcolumn for username
    Signed-off-by: Shane Bryzak <sbryzak@gmail.com>

--- pkg/apis/toolchain/v1alpha1/usersignup_types.go
+++ pkg/apis/toolchain/v1alpha1/usersignup_types.go
@@ -61,6 +61,7 @@ type UserSignupStatus struct {
 // +k8s:openapi-gen=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="User ID",type="string",JSONPath=".spec.userID",priority=1
+// +kubebuilder:printcolumn:name="Username",type="string",JSONPath=".spec.username",priority=1
 // +kubebuilder:printcolumn:name="TargetCluster",type="string",JSONPath=".spec.targetCluster",priority=1
 // +kubebuilder:printcolumn:name="Complete",type="string",JSONPath=".status.conditions[?(@.type=="Complete")].status"
 // +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=".status.conditions[?(@.type=="Complete")].reason"
