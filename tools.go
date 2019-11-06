a13826c7b1c0a603f4821ab69a85f73293cf072e
Alexey Kazakov <alkazako@redhat.com>
2019-11-06 07:50:41 -0800

    Generate KubeFedCluster CRD

--- tools.go
+++ tools.go
@@ -4,6 +4,7 @@ package tools
 
 import (
 	// Code generators built at runtime.
+	_ "github.com/go-bindata/go-bindata"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/conversion-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
