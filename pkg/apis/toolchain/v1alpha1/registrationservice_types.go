a2d5a437badb44377027c3f28d7434b26c404113
Matous Jobanek <mjobanek@redhat.com>
2019-11-26 11:28:47 +0000

    feat(CRT-352): create RegistrationService CRD
    it will be used as a holder of reg-service configuration as well as a
    triggerrer of the actual deployment

---
+++ pkg/apis/toolchain/v1alpha1/registrationservice_types.go
@@ -0,0 +1,83 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RegistrationServiceSpec defines the desired state of RegistrationService
+// +k8s:openapi-gen=true
+type RegistrationServiceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// The image identifies which image of the registration service should be used for a deployment
+	Image string `json:"version"`
+
+	// The number of replicas of the deployed registration service
+	Replicas int `json:"replicas,omitempty"`
+
+	// The environment identifies which mode the registration service should be running in - prod, stage, e2e-tests, dev, etc.
+	Environment string `json:"environment,omitempty"`
+
+	// The AuthClient contains all necessary information about the auth client
+	AuthClient AuthClient `json:"authClient,omitempty"`
+}
+
+type AuthClient struct {
+	// The LibraryUrl identifies the auth library location
+	LibraryUrl string `json:"libraryUrl,omitempty"`
+
+	// The Config contains the auth config
+	Config string `json:"config,omitempty"`
+
+	// The PublicKeysUrl identifies the public keys location
+	PublicKeysUrl string `json:"publicKeysUrl,omitempty"`
+}
+
+// RegistrationServiceStatus defines the observed state of RegistrationService
+// +k8s:openapi-gen=true
+type RegistrationServiceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Conditions is an array of current Registration Service deployment conditions
+	// Supported condition types:
+	// Deploying, and Deployed
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType
+	Conditions []Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RegistrationService is the Schema for the registrationservices API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=registrationservices,scope=Namespaced
+type RegistrationService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RegistrationServiceSpec   `json:"spec,omitempty"`
+	Status RegistrationServiceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RegistrationServiceList contains a list of RegistrationService
+type RegistrationServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RegistrationService `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RegistrationService{}, &RegistrationServiceList{})
+}
