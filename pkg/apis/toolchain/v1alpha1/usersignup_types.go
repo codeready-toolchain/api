1c0e0f52cb84b852d1be9f39972c6adc70a87fd8
tinakurian <tkurian@redhat.com>
2019-12-03 19:24:54 -0500

    adding email

--- pkg/apis/toolchain/v1alpha1/usersignup_types.go
+++ pkg/apis/toolchain/v1alpha1/usersignup_types.go
@@ -61,13 +61,14 @@ type UserSignupStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:printcolumn:name="User ID",type="string",JSONPath=`.spec.userID`,priority=1
-// +kubebuilder:printcolumn:name="Username",type="string",JSONPath=`.spec.username`,priority=1
+// +kubebuilder:printcolumn:name="Username",type="string",JSONPath=`.spec.username`
 // +kubebuilder:printcolumn:name="TargetCluster",type="string",JSONPath=`.spec.targetCluster`,priority=1
 // +kubebuilder:printcolumn:name="Complete",type="string",JSONPath=`.status.conditions[?(@.type=="Complete")].status`
 // +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=`.status.conditions[?(@.type=="Complete")].reason`
 // +kubebuilder:printcolumn:name="Approved",type="string",JSONPath=`.status.conditions[?(@.type=="Approved")].status`,priority=1
 // +kubebuilder:printcolumn:name="ApprovedBy",type="string",JSONPath=`.status.conditions[?(@.type=="Approved")].reason`,priority=1
 // +kubebuilder:printcolumn:name="CompliantUsername",type="string",JSONPath=`.status.compliantUsername`
+// +kubebuilder:printcolumn:name="Email",type="string",JSONPath=`.meta.annotations[?(@.type=="Email")]`
 type UserSignup struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
