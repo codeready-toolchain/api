ef48cc411b715e4ad62794c0383c5558b95035b2
Xavier Coulon <xcoulon@redhat.com>
2020-06-09 17:14:52 +0200

    feat: TemplateUpdateRequest type
    TemplateUpdateRequest resource carry the name
    of the MasterUserRecord resource which will be updated
    after an NStemplateTier changed
    
    Fixes https://issues.redhat.com/browse/CRT-683
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ pkg/apis/toolchain/v1alpha1/templateupdaterequest_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NSTemplateTierNameLabelKey stores the name of NSTemplateTier that this TemplateUpdateRequest is related to
+const NSTemplateTierNameLabelKey = LabelKeyPrefix + "nstemplatetier"
+
+// TemplateUpdateRequestSpec defines the desired state of TemplateUpdateRequest
+// It contains the new TemplateRefs to
+type TemplateUpdateRequestSpec struct {
+	TierName           string           `json:"tierName"`
+	NSTemplateTierSpec `json:",inline"` // TODO: factorize this struct with NSTemplateTierSpec and NSTemplateSetSpec
+}
+
+// TemplateUpdateRequestStatus defines the observed state of TemplateUpdateRequest
+type TemplateUpdateRequestStatus struct {
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TemplateUpdateRequest is the Schema for the templateupdaterequests API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=templateupdaterequests,scope=Namespaced
+type TemplateUpdateRequest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TemplateUpdateRequestSpec   `json:"spec,omitempty"`
+	Status TemplateUpdateRequestStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TemplateUpdateRequestList contains a list of TemplateUpdateRequest
+type TemplateUpdateRequestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TemplateUpdateRequest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TemplateUpdateRequest{}, &TemplateUpdateRequestList{})
+}
