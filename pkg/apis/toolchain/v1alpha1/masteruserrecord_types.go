955aff6a62eeef5479d045184ed814467b383c74
Rajiv Senthilnathan <rsenthil@redhat.com>
2020-08-18 23:01:03 -0400

    Remove provision time label since haven't decided its use yet

--- pkg/apis/toolchain/v1alpha1/masteruserrecord_types.go
+++ pkg/apis/toolchain/v1alpha1/masteruserrecord_types.go
@@ -31,7 +31,6 @@ const (
 	MasterUserRecordNotificationCRCreatedReason              = "NotificationCRCreated"
 	MasterUserRecordNotificationCRCreationFailedReason       = "NotificationCRCreationFailed"
 	MasterUserRecordUserIDLabelKey                           = LabelKeyPrefix + "user-id"
-	MasterUserRecordProvisionedTimeUNIXLabelKey              = LabelKeyPrefix + "provisioned-time-unix"
 )
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
