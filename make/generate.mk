6d54e13848d45dd84278ca993513516afc8fc38e
Rajiv Senthilnathan <jeevandroid@gmail.com>
2021-09-03 09:04:35 -0400

    Update make/generate.mk
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

--- make/generate.mk
+++ make/generate.mk
@@ -4,7 +4,7 @@ API_FULL_GROUPNAME=toolchain.dev.openshift.com
 API_VERSION:=v1alpha1
 
 # how to dispatch the CRD files per repository (space-separated lists)
-# !!! IMPORTANT !!! - when there is a new CRD added or an existing one removed or renamed, don't forget to change it also here: https://github.com/codeready-toolchain/toolchain-common/blob/master/scripts/add-cluster.sh#L54-L69
+# !!! IMPORTANT !!! - when there is a new CRD added or an existing one removed or renamed, don't forget to change it also here: https://github.com/codeready-toolchain/toolchain-cicd/blob/master/scripts/add-cluster.sh#L52-L73
 HOST_CLUSTER_CRDS:=masteruserrecords nstemplatetiers usersignups bannedusers changetierrequests notifications tiertemplates templateupdaterequests toolchainstatuses toolchainclusters toolchainconfigs
 MEMBER_CLUSTER_CRDS:=useraccounts nstemplatesets memberstatuses idlers toolchainclusters memberoperatorconfigs
 
