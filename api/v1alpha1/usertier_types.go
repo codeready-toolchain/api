9b426a7e51372a09867da6b199a836bc5ab5d028
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-04-20 10:59:40 -0400

    Remove status altogether

--- api/v1alpha1/usertier_types.go
+++ api/v1alpha1/usertier_types.go
@@ -17,8 +17,7 @@ type UserTier struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   UserTierSpec   `json:"spec,omitempty"`
-	Status UserTierStatus `json:"status,omitempty"`
+	Spec UserTierSpec `json:"spec,omitempty"`
 }
 
 // UserTierSpec defines the desired state of UserTier
@@ -30,30 +29,6 @@ type UserTierSpec struct {
 	// the period (in days) after which users within the tier will be deactivated
 	// +optional
 	DeactivationTimeoutDays int `json:"deactivationTimeoutDays,omitempty"`
-}
-
-// UserTierStatus defines the observed state of UserTier
-// +k8s:openapi-gen=true
-type UserTierStatus struct {
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-
-	// Conditions is an array of current UserTier conditions
-	// Supported condition types: ConditionReady
-	// +optional
-	// +patchMergeKey=type
-	// +patchStrategy=merge
-	// +listType=map
-	// +listMapKey=type
-	Conditions []Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
-}
-
-// UserTierHistory a track record of an update
-type UserTierHistory struct {
-	// StartTime is the time when the UserTier was updated
-	UpdateTime metav1.Time `json:"startTime"`
-	// Hash the hash matching of the UserTier spec
-	Hash string `json:"hash"`
 }
 
 //+kubebuilder:object:root=true
