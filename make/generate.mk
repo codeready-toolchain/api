654249eee4819a3e7e1cd7c8f88e3a12ebb77dd9
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-04-19 23:24:52 -0400

    Add to Host cluster CRDs

--- make/generate.mk
+++ make/generate.mk
@@ -5,7 +5,7 @@ API_VERSION:=v1alpha1
 
 # how to dispatch the CRD files per repository (space-separated lists)
 # !!! IMPORTANT !!! - when there is a new CRD added or an existing one removed or renamed, don't forget to change it also here: https://github.com/codeready-toolchain/toolchain-cicd/blob/master/scripts/add-cluster.sh#L52-L73
-HOST_CLUSTER_CRDS:=masteruserrecords nstemplatetiers usersignups bannedusers changetierrequests notifications spaces spacebindings socialevents tiertemplates templateupdaterequests toolchainstatuses toolchainclusters toolchainconfigs
+HOST_CLUSTER_CRDS:=masteruserrecords nstemplatetiers usersignups bannedusers changetierrequests notifications spaces spacebindings socialevents tiertemplates templateupdaterequests toolchainstatuses toolchainclusters toolchainconfigs usertiers
 MEMBER_CLUSTER_CRDS:=useraccounts nstemplatesets memberstatuses idlers toolchainclusters memberoperatorconfigs
 
 PATH_TO_CRD_BASES=config/crd/bases
