1cfba10ff88199eb67263793274ef3492d0b7d55
Francisc Munteanu <fmuntean@redhat.com>
2023-01-10 10:39:10 +0100

    add parentSpaceLabelKey in space_types.go

--- api/v1alpha1/space_types.go
+++ api/v1alpha1/space_types.go
@@ -10,6 +10,10 @@ const (
 
 	// WorkspaceLabelKey is used to label the Space with the name of the associated AppStudio Workspace
 	WorkspaceLabelKey = LabelKeyPrefix + "workspace"
+
+	// ParentSpaceLabelKey is used to label the Space with the name of the parent space
+	// from which the creation was requested
+	ParentSpaceLabelKey = LabelKeyPrefix + "parent-space"
 )
 
 // These are valid status condition reasons of a Space
