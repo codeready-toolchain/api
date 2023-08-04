047b143a4d4eba531cfe7d821366dd8ef44aafd9
Francisc Munteanu <fmuntean@redhat.com>
2023-08-04 14:08:29 +0200

    add spacebindingrequest type to host and member operators (#370)

--- make/generate.mk
+++ make/generate.mk
@@ -5,8 +5,8 @@ API_VERSION:=v1alpha1
 
 # how to dispatch the CRD files per repository (space-separated lists)
 # !!! IMPORTANT !!! - when there is a new CRD added or an existing one removed or renamed, don't forget to change it also here: https://github.com/codeready-toolchain/toolchain-cicd/blob/master/scripts/add-cluster.sh#L52-L73
-HOST_CLUSTER_CRDS:=masteruserrecords nstemplatetiers usersignups bannedusers notifications spaces spacebindings socialevents tiertemplates toolchainstatuses toolchainclusters toolchainconfigs usertiers proxyplugins spacerequests
-MEMBER_CLUSTER_CRDS:=useraccounts nstemplatesets memberstatuses idlers toolchainclusters memberoperatorconfigs spacerequests workspaces
+HOST_CLUSTER_CRDS:=masteruserrecords nstemplatetiers usersignups bannedusers notifications spaces spacebindings socialevents tiertemplates toolchainstatuses toolchainclusters toolchainconfigs usertiers proxyplugins spacerequests spacebindingrequests
+MEMBER_CLUSTER_CRDS:=useraccounts nstemplatesets memberstatuses idlers toolchainclusters memberoperatorconfigs spacerequests workspaces spacebindingrequests
 
 PATH_TO_CRD_BASES=config/crd/bases
 
