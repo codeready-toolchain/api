aa255bbb3941ae4278d93055203d39333244bbe4
Shane Bryzak <sbryzak@gmail.com>
2024-01-06 20:02:20 +1000

    review comments

--- api/v1alpha1/useraccount_types.go
+++ api/v1alpha1/useraccount_types.go
@@ -16,6 +16,21 @@ const (
 	UserAccountDisablingReason              = "Disabling"
 	UserAccountTerminatingReason            = terminatingReason
 	UserAccountUpdatingReason               = updatingReason
+
+	// AnnotationKeyPrefix is the prefix used for annotation key values
+	AnnotationKeyPrefix = LabelKeyPrefix
+
+	// UserIDUserAnnotationKey is used to set an annotation value in the User resource on the member cluster, that
+	// contains the user's User ID as set in the user's JWT token.
+	UserIDUserAnnotationKey = AnnotationKeyPrefix + "sso-user-id"
+
+	// AccountIDUserAnnotationKey is used to set an annotation value in the User resource on the member cluster, that
+	// contains the user's Account ID as set in the user's JWT token.
+	AccountIDUserAnnotationKey = AnnotationKeyPrefix + "sso-account-id"
+
+	// EmailUserAnnotationKey is used to set an annotation value in the User resource on the member cluster, that
+	// contains the user's Email as set in the user's JWT token.
+	EmailUserAnnotationKey = AnnotationKeyPrefix + "email"
 )
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
