c21889595d2e5f232b2b34850577e21baf4b260f
Lukas Krejci <lkrejci@redhat.com>
2024-05-16 17:52:38 +0200

    Add a new SPC Ready reason.

--- api/v1alpha1/spaceprovisionerconfig_types.go
+++ api/v1alpha1/spaceprovisionerconfig_types.go
@@ -4,6 +4,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 const (
 	SpaceProvisionerConfigToolchainClusterNotFoundReason = "ToolchainClusterNotFound"
+	SpaceProvisionerConfigToolchainClusterNotReadyReason = "ToolchainClusterNotReady"
 	SpaceProvisionerConfigValidReason                    = "AllChecksPassed"
 )
 
