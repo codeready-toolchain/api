18493eae06c6759a5c915025d06e393040b62c22
Francisc Munteanu <fmuntean@redhat.com>
2024-05-29 11:51:05 +0200

    update api version

--- api/v1alpha1/zz_generated.openapi.go
+++ api/v1alpha1/zz_generated.openapi.go
@@ -5213,7 +5213,7 @@ func schema_codeready_toolchain_api_api_v1alpha1_WebhookConfig(ref common.Refere
 				Properties: map[string]spec.Schema{
 					"deploy": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Defines the flag that determines whether to deploy the Webhook. If the deploy flag is disabled, the Webhook is deployed and deleted immediately after by the memberoperatorconfig controller.",
+							Description: "Defines the flag that determines whether to deploy the Webhook. If the deploy flag is set to False and the Webhook was deployed previously it will be deleted by the memberoperatorconfig controller.",
 							Type:        []string{"boolean"},
 							Format:      "",
 						},
