9469259af54cccc8f735274a60de3691144ecf50
Alexey Kazakov <alkazako@redhat.com>
2024-06-24 17:14:19 -0700

    reorg

--- api/v1alpha1/featutetoggles.go
+++ api/v1alpha1/featutetoggles.go
@@ -1,11 +1,11 @@
 package v1alpha1
 
 const (
+	// FeatureToggleNameAnnotationKey is used for referring tier template objects to feature toggles defined in configuration
+	FeatureToggleNameAnnotationKey = LabelKeyPrefix + "feature"
+
 	// FeatureAnnotationKeyPrefix is used in feature annotation keys in Space, NSTemplate and other resources
 	// to refer to the corresponding feature toggle from the configuration:
 	// "toolchain.dev.openshift.com/feature/<feature-name>
 	FeatureAnnotationKeyPrefix = FeatureToggleNameAnnotationKey + "/"
-
-	// FeatureToggleNameAnnotationKey is used for referring tier template objects to feature toggles defined in configuration
-	FeatureToggleNameAnnotationKey = LabelKeyPrefix + "feature"
 )
