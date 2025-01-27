91d17fc2713863e7e6596f41b8f6a479b78ebc02
Feny Mehta <fbm3307@gmail.com>
2025-01-27 17:43:58 +0530

    a silly mis of $ sign
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- scripts/verify-replace.sh
+++ scripts/verify-replace.sh
@@ -63,9 +63,9 @@ if [ ${#ERRORREPOLIST[@]} -ne 0 ]; then
                 for s in ${STDOUTFILELIST[*]}
                 do
                     if [[ ${s} =~ ${e} ]]; then 
-                        cat "${s}" | grep -E ${GOLINTREGEX} > LINTERERRORFILE
-                        if ! [ -s "LINTERERRORFILE" ]; then
-                            cat  LINTERERRORFILE   
+                        cat "${s}" | grep -E ${GOLINTREGEX} > ${LINTERERRORFILE}
+                        if ! [ -s "${LINTERERRORFILE}" ]; then
+                            cat  "${LINTERERRORFILE}"  
                         fi
                     fi
                 done                                            
